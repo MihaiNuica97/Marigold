242bfb00d6c626004f4bc93a792f8575a9ddf4dc
MihaiNuica97 <mihainuica@gmail.com>
2020-06-23 17:41:53 +0100

    contact form first iteration

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+echo("Sending...");
+$to = "mihainuica@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+echo("Sent");
+?>
