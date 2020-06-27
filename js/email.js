cb5457d3a556a56c1ecbfe31e009b241164481f9
MihaiNuica97 <mihainuica@gmail.com>
2020-06-27 16:20:35 +0100

    added submit sequence

--- js/email.js
+++ js/email.js
@@ -1,4 +1,11 @@
 function sendEmail(){
+    $("#contact-form-outer").toggleClass("hidden");
+    $('#final-message').addClass('show');
+    
+    setTimeout(function(){
+        $('#contact-popup').toggleClass('open'); 
+    },1500);
+
     let name = $("#name").val();
     let email = $("#email").val();
     let subject = $("#subject").val();
@@ -13,7 +20,7 @@ function sendEmail(){
     }
     packet = JSON.stringify(packet);
     console.log("sending");
-    $.post('http://mihainuica.com/marigold/mail.php', packet,  function(response){
-        console.log(JSON.parse(response));
-    });
+    // $.post('http://mihainuica.com/marigold/mail.php', packet,  function(response){
+    //     console.log(JSON.parse(response));
+    // });
 }
