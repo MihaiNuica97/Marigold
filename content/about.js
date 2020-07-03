a8fe3c567adba8c40a03931487188da02b21580c
MihaiNuica97 <mihainuica@gmail.com>
2020-07-03 16:52:54 +0100

    most data moved to external files

---
+++ content/about.js
@@ -0,0 +1,11 @@
+const about = {
+    img:{
+        top:"./img/about.jpg"
+    },
+    // fiecare sectiune in ghilimele este un paragraf. Toate sectiunile trebuie SEPARATE CU VIRGULA si puse intre ghilimele
+    txt:[
+        "Numele meu este Alina Valeria Gherda si sunt bucuroasa ca pot sa iti ofer expertiza mea pentru servicii de salon in cosmetica, machiaj si coafura. Pentru mine valoroasa este bucuria ta de a reusi sa te simti bine, sa te privesti cu drag in oglinda si sa fii incantata de aspectul tau.",
+        "Ma simt fericita sa daruiesc incredere, stare de bine si calitate la superlativ in tot ceea ce fac. Placerea mea de a preda o pun in valoare creand lectii particulare in functie de necesitatile de invatare si prioritatile cursantilor si in cursurl de formare profesionala pentru meseria de Machior pe care l-am acreditat.",
+        "Te pot ajuta sa descoperi si sa experimentezi frumusetea ta in lectii particulare unu la unu atunci cand doresti o schimbare de look si cand prioritatea ta devii tu in aspect,atitudine si in modul cum doresti sa fii privita de ceilalti. Te astept cu drag sa ne cunoastem si iti multumesc ca alegi  Marigold."
+    ]
+};
---
+++ content/offers.js
@@ -0,0 +1,45 @@
+const offers = {
+    // fiecare element este incadrat in {} si separat de virgula
+    txt:[
+        {
+            title:"Tunsoare fara schimbare de look + coafura de zi",
+            list:[
+                "Par Scurt: 50 Lei",
+                "Par Mediu: 70 Lei",
+                "Par Lung: 100 Lei"
+            ]
+        },
+        {
+            title:"Tunsoare cu schimbare de look + coafura de zi",
+            list:[
+                "Par Scurt: 60 Lei",
+                "Par Mediu: 100 Lei",
+                "Par Lung: 120 Lei"
+            ]
+        },
+        {
+            title:"Coafura de zi + machiaj de zi",
+            list:[
+                "Par Scurt: 100 Lei",
+                "Par Mediu: 120 Lei",
+                "Par Lung: 150 Lei"
+            ]
+        },
+        {
+            title:"Tunosare fara schimbare de look + coafura de zi + machiaj de zi",
+            list:[
+                "Par Scurt: 150 Lei",
+                "Par Mediu: 170 Lei",
+                "Par Lung: 190 Lei"
+            ]
+        },
+        {
+            title:"Tunosare cu schimbare de look + coafura de zi + machiaj de zi",
+            list:[
+                "Par Scurt: 170 Lei",
+                "Par Mediu: 190 Lei",
+                "Par Lung: 210 Lei"
+            ]
+        }
+    ]
+}
