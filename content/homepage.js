1ed115b979b38cef228ad78724cc55ff46b96d14
MihaiNuica97 <mihainuica@gmail.com>
2020-07-03 16:04:27 +0100

    homepage content moved to external js file

---
+++ content/homepage.js
@@ -0,0 +1,16 @@
+const homepage = {
+    img:{
+        fata:{
+            stanga:"./img/homepage-fg-1.jpg",
+            dreapta:"./img/homepage-fg-2.jpg"
+        },
+        spate:{
+            stanga:"./img/homepage-bg-1.jpg",
+            dreapta:"./img/homepage-bg-2.jpg"
+        }
+    },
+    text:{
+        stanga:"Devino versiunea pe care iti doresti sa o vezi in oglinda. Marigold este aici ca sa iti ofere servicii de cea mai inalta calitate.",
+        dreapta:"La Marigold se folosec doar produsele unor branduri de top care satisfac asteptarile clientilor"
+    }
+};
