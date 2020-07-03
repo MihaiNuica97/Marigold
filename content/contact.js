d03b442f2e83e0fa5d5eb76f63f61c237a9ab48d
MihaiNuica97 <mihainuica@gmail.com>
2020-07-03 15:40:12 +0100

    moved footer data to separate js file

---
+++ content/contact.js
@@ -0,0 +1,14 @@
+const contact = {
+    tel:"0723445054",
+    adresa:{
+        strada:"Str. George Cosbuc nr. 13",
+        oras: "Ploiesti, Romania",
+        postcode: "Cod Postal: 107071",
+    },
+    // fiecare linie este o linie pe pagina
+    program:[
+        "Luni-Vineri: 9:00-11:00 14:00-19:00",
+        "Sambata: 10:00-14:00",
+        "Duminica: Inchis"
+    ]
+};
